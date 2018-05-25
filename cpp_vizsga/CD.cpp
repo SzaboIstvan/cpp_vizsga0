6706221ddc84cdaf7d301fb042e77a8aa5d3e4b4
Szabó István <istvan.szabo.1992@gmail.com>
2018-05-25 15:53:52 +0200

    recorder

--- cpp_vizsga/CD.cpp
+++ cpp_vizsga/CD.cpp
@@ -17,4 +17,5 @@ void CD::Input(char * _type, char * _name, int _capacity)
 
 void CD::Print()
 {
+	Recorder::Print();
 }
--- cpp_vizsga/Recorder.cpp
+++ cpp_vizsga/Recorder.cpp
@@ -1,6 +1,8 @@
+#include <string.h>
+#include <iostream>
 #include "Recorder.h"
 
-
+using namespace std;
 
 Recorder::Recorder()
 {
@@ -9,12 +11,20 @@ Recorder::Recorder()
 
 Recorder::~Recorder()
 {
+	delete[] type;
+	delete[] name;
 }
 
-void Recorder::Input(char * type, char * name)
+void Recorder::Input(char * _type, char * _name)
 {
+	type = new char[10];
+	strcpy(type, _type);
+
+	name = new char[10];
+	strcpy(name, _name);
 }
 
 void Recorder::Print()
 {
+	cout << "Type: " << type << " Name: " << name;
 }
